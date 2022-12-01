837aec05a5e13e4d5be48e5c577faad4e7da1ad9
i.shirinov <shirinovilyos6965@gmail.com>
2022-12-01 15:58:17 +0500

    FINAL MODULE TASK 1. Season determiner.

--- src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/BitwiseValuesSwap.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class BitwiseValuesSwap {
     public void swap(int first, int second) {
+        first = first ^ second;
+        second = first ^ second;
+        first = first ^ second;
+        System.out.println(first + "\n" + second);
+    }
+
+    public static void main(String[] args) {
+        BitwiseValuesSwap swap=new BitwiseValuesSwap();
+        swap.swap(5,2);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        if (divider == 0) {
+            System.out.println("division by zero");
+        } else if (dividend % divider != 0) {
+            System.out.println("cannot be divided completely");
+        } else if (dividend % divider == 0) {
+            System.out.println("can be divided completely");
+        }
+    }
+
+    public static void main(String[] args) {
+        IntegerDivider divider = new IntegerDivider();
+        divider.printCompletelyDivided(1, 1);
     }
 }
